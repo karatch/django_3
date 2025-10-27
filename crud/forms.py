fa2afc3e5b88fc86d443d17b3f7e85c82aa5d032
Alexey Karatchentsev <karatchentsev@yandex.ru>
2025-10-27 21:02:57 +0300

    tested forms

--- crud/forms.py
+++ crud/forms.py
@@ -3,8 +3,8 @@ from django.forms import SelectDateWidget
 
 
 class UserForm(forms.Form):
-    name = forms.CharField(max_length=12, label='Имя', initial='John', help_text='Введите Ваше имя')
-    last_name = forms.CharField(label='Фамилия', initial='Doe')
-    age = forms.IntegerField(label='Ваш возраст', initial=18)
-    married = forms.BooleanField(initial=False, required=False)
-    date = forms.DateField(widget=SelectDateWidget)
+    name = forms.CharField(label='Имя', initial='undefined', help_text='Введите свое имя')
+    age = forms.IntegerField(
+        label='Ваш возраст', initial=16, widget=forms.Textarea, help_text='Введите свой возраст'
+    )
+    field_order = ['age', 'name']
--- crud/views.py
+++ crud/views.py
@@ -9,7 +9,7 @@ def index(request):
         name = request.POST.get('name')
         age = request.POST.get('age')
         last_name = request.POST.get('last_name')
-        return HttpResponse(f'<h2>Привет, {name} {last_name}! Твой возраст: {age}</h2>')
+        return HttpResponse(f'<h2>Привет, {name}! Твой возраст: {age}</h2>')
     else:
         userform = UserForm()
         return render(request, 'index.html', {'form': userform})
